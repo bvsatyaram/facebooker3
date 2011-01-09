5e3e7ffba6dfcb73d4cffbe61b877d5e759ecf1b
B V Satyaram <bvsatyaram@gmail.com>
2011-01-10 00:32:42 +0530

    Fixing authorization logic. Actually a typo

--- app/controllers/facebookerauth_controller.rb
+++ app/controllers/facebookerauth_controller.rb
@@ -2,6 +2,7 @@ class FacebookerauthController < ActionController::Base
   skip_before_filter :authorization_required, :only => [:authorize, :callback]
   
   def authorize
+    # TODO: Skip authorization if no default permissions exist
     @reauthorize = (params[:authorized] == "false")
     @authorization_url = oclient.web_server.authorize_url(:redirect_uri => facebookerauth_callback_url, :scope => Facebooker3.facebooker3_config["default_permissions"])
   end
@@ -10,7 +11,6 @@ class FacebookerauthController < ActionController::Base
     if params[:error]
       redirect_to (ENV['FACEBOOK_APP_URL'] + "facebookerauth/authorize?authorized=false")
     else
-      debugger
       #FIXME: The following call is throwing an exception:
       # OAuth2::HTTPError Exception: Received HTTP 400 during request.
       oauth_response = oclient.web_server.get_access_token(params[:code], :redirect_uri => facebookerauth_callback_url)
@@ -22,6 +22,6 @@ class FacebookerauthController < ActionController::Base
   protected
 
   def oclient
-    @oclient ||= OAuth2::Client.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], :site => ENV['GRAPH_URL'])
+    @oclient ||= OAuth2::Client.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET_KEY'], :site => ENV['GRAPH_URL'])
   end
 end
